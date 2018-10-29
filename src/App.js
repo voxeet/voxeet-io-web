98c1d9472b7594bd58ba87b6f24cc485b4617398
Valentin Martin <valentin.martin33@gmail.com>
2018-10-29 16:24:07 +0100

    delete debug

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ class App extends Component {
 
   componentWillMount() {
     const { conferenceName } = this.props.match.params
-    console.error(this.props);
     if (conferenceName) {
       this.setState({ isJoiningFromUrl: true, form :{ conferenceName: conferenceName}})
     }
