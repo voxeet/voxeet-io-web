4732ad1ea626fb030a6d31175fc690a92f2e1720
Thomas <thomas.gourgues@4thlabs.io>
2018-10-30 10:32:50 +0100

    Fixes for electron and react

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ app.on('window-all-close', () => {
 });
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1280, height: 720});
   mainWindow.webContents.setFrameRate(30);
   
   ipcMain.on('leave', (e) => {
